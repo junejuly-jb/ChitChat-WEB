aad9d34f5cb7a0ff5d8490f1f3420cb03565764c
JBA <junearagoncillo@gmail.com>
2022-11-06 13:26:58 +0800

    change routes to not lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-const HomeView = () => import('../views/HomeView.vue')
-const LoginView = () => import('../views/LoginView.vue')
+// const HomeView = () => import('../views/HomeView.vue')
+// const LoginView = () => import('../views/LoginView.vue')
+
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
