e38f8f1e55ea7c60d288f757cd11056e810465a7
JBA <junearagoncillo@gmail.com>
2022-11-23 22:15:31 +0800

    added hover item file

---
+++ src/components/HoverItems.vue
@@ -0,0 +1,22 @@
+<script>
+
+</script>
+<template>
+    <div class="px-2">
+        <button class="px-1">
+            <v-icon>mdi-emoticon</v-icon>
+        </button>
+        <button class="px-1">
+            <v-icon>mdi-reply</v-icon>
+        </button>
+        <button class="px-1">
+            <v-icon>mdi-dots-vertical</v-icon>
+        </button>
+    </div>
+</template>
+
+<style>
+    .v-icon{
+        color: lightgray;
+    }
+</style>
